d4c50de56998b3a0b7be08af557f9205dcfb3f3b
5surabbit5 <2318737638@qq.com>
2020-12-30 21:57:42 +0800

    '提交'

---
+++ src/classComponent.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './vote.less';
+class TestLifeCycle extends React.Component {
+  static defaultProps = {
+    num: 100,
+  };
+  static propTypes = {
+    num: PropTypes.number,
+  };
+  constructor(props) {
+    // console.log('constructor');
+    super(props);
+    this.state = {
+      num: this.props.num,
+    };
+    console.log(this.props);
+  }
+  // componentWillMount() {
+  //   console.log('componentWillMount');
+  // }
+  static getDerivedStateFromProps(props, state) {
+    console.log('getDreivedStateFromProps', props, state);
+    return props;
+  }
+
+  // componentDidMount() {
+  //   console.log('componentDidMount');
+  // }
+
+  // handleClick = () => {
+  //   console.log('1');
+  //   this.setState({
+  //     num: this.state.num + 1,
+  //   });
+  // };
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('shouldComponentUpdate', nextProps, nextState);
+  //   return true;
+  // }
+
+  // componentWillUpdate() {
+  //   console.log('componentWillUpdate');
+  // }
+  // getSnapshotBeforeUpdate(prevProps, prevState) {
+  //   console.log('getSnapshotBeforeUpdate', prevProps, prevState);
+  //   return null;
+  // }
+  // componentDidUpdate() {
+  //   console.log('componentDidMount');
+  // }
+  // componentWillReceiveProps(nextProps) {
+  //   console.log('componentWillReceiveProps', nextProps);
+  // }
+  render() {
+    console.log('render');
+    return (
+      <div onClick={() => this.handleClick()} className="vote-box">
+        {this.state.num}
+      </div>
+    );
+  }
+}
+
+export default class ControlCenter extends React.Component {
+  state = {
+    x: 0,
+  };
+  handleClick = () => {
+    const { x } = this.state;
+    this.setState({
+      x: x + 1,
+    });
+  };
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('shouldComponentUpdate', nextProps, nextState);
+  //   return true;
+  // }
+  render() {
+    const { x } = this.state;
+    return (
+      <div>
+        <TestLifeCycle num={x}></TestLifeCycle>
+        <button onClick={() => this.handleClick()}>点我加一</button>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import ControlCenter from './classComponent';
+// import TestLifeCycle from './classComponent';
+// import Vote from './reactComponent';
+// import TestLiftCycle from './classComponent';
+// import ControlCenter from './classComponent';
+// import { createElement, render } from './SelfJSX';
+// import App from './App';
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ControlCenter></ControlCenter>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// console.log(
+//   React.createElement(
+//     'div',
+//     null,
+//     '\u73E0\u5CF0\u57F9\u8BAD',
+//     React.createElement(Vote, null)
+//   )
+// );
+{
+  /* <div>
+珠峰培训
+<Vote index={10} title="努力学习">
+  <span style={{ fontSize: '35px' }}>里面的元素</span>
+</Vote>
+</div>, */
+}
+{
+  /* <div className="box" id="box" style={{ color: 'red' }}>
+你好世界
+<span>aaa</span>
+</div> */
+}
+// const realDom = createElement(
+//   'div',
+//   {
+//     className: 'box',
+//     id: 'box',
+//     index: '1',
+//     style: {
+//       color: 'red',
+//       fontSize: '28px',
+//     },
+//   },
+//   '测试文字',
+//   createElement('span', null, 'aaa')
+// );
+// console.log(realDom);
+// render(realDom, document.getElementById('root'), () => {
+//   console.log('页面渲染完成了');
+// });
